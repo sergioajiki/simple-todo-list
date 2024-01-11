13138f068ae7d46860662706b2907dcd84079541
Sergio Ajiki <sergioajk@hotmail.com>
2024-01-11 15:57:52 -0300

    feat: entity task

--- src/main/java/com/simpletodolist/simpletodolist/controller/HealthController.java
+++ src/main/java/com/simpletodolist/simpletodolist/controller/HealthController.java
@@ -1,14 +1,16 @@
 package com.simpletodolist.simpletodolist.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Health")
 @RequestMapping("/health")
 public class HealthController {
     @GetMapping
     public static String sayHello() {
-        return "Success!!";
+        return "To Do List - Success!!";
     }
 }
---
+++ src/main/java/com/simpletodolist/simpletodolist/entity/Task.java
@@ -0,0 +1,59 @@
+package com.simpletodolist.simpletodolist.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+    private String taskName;
+    private String description;
+    private LocalDate taskCreationDate;
+    private String Priority;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getTaskCreationDate() {
+        return taskCreationDate;
+    }
+
+    public void setTaskCreationDate(LocalDate taskCreationDate) {
+        this.taskCreationDate = taskCreationDate;
+    }
+
+    public String getPriority() {
+        return Priority;
+    }
+
+    public void setPriority(String priority) {
+        Priority = priority;
+    }
+
+}
