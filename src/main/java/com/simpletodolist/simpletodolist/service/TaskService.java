4928c1f183ea1132de7f3f95b8ddf7e613ea7f9b
Sergio Ajiki <sergioajk@hotmail.com>
2024-01-11 16:35:27 -0300

    feat: task Service

---
+++ src/main/java/com/simpletodolist/simpletodolist/service/TaskService.java
@@ -0,0 +1,15 @@
+package com.simpletodolist.simpletodolist.service;
+
+import com.simpletodolist.simpletodolist.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+}
