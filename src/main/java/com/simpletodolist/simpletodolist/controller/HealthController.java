aaa8961740f3026575c3f23d2e7a1f53787643d7
Sergio Ajiki <sergioajk@hotmail.com>
2024-01-11 14:53:54 -0300

    feat: rota health

--- src/main/java/com/simpletodolist/simpletodolist/controller/HealthController.java
+++ src/main/java/com/simpletodolist/simpletodolist/controller/HealthController.java
@@ -1,4 +1,14 @@
 package com.simpletodolist.simpletodolist.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
 public class HealthController {
+    @GetMapping
+    public static String sayHello() {
+        return "Success!!";
+    }
 }
